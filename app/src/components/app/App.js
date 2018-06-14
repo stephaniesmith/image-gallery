47743c2ad03c09100e200b4cbd3ef61041cdf8eb
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-14 10:01:58 -0700

    hello world main front end routes

--- app/src/components/app/App.js
+++ app/src/components/app/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import Albums from '../albums/Albums';
+import About from '../about/About';
+import Images from '../images/Images';
 
 export default class App extends Component {
   render() {
@@ -11,7 +14,11 @@ export default class App extends Component {
         <div>
           <Header/>
           <main>
-            Hello World
+            <Switch>
+              <Route path="/albums" component={Albums}/>
+              <Route path="/about" component={About}/>
+              <Route path="/images" component={Images}/>
+            </Switch>
           </main>
           <Footer/>
         </div>
