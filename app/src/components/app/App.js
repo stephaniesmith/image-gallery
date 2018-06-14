8de297a18756fe0c31ea49d6d6aa2a8e191a940a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-14 09:49:45 -0700

    setting up basic app structure

--- app/src/components/app/App.js
+++ app/src/components/app/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      
+      <Router>
+        <div>
+          <Header/>
+          <main>
+            Hello World
+          </main>
+          <Footer/>
+        </div>
+      </Router>
     );
   }
 }
