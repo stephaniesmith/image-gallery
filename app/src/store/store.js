d82a14a785fd8dc32a3243b097282d6158c45ce4
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-15 10:45:16 -0700

    added albums to the store

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { albums } from '../components/albums/reducers';
 
 const rootReducer = combineReducers({
-
+  albums
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
