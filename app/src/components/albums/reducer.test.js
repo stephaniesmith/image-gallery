3a8ec22428741e82a29440f3f97c93baf2c6ac21
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-15 06:50:56 -0700

    selector tests

--- app/src/components/albums/reducer.test.js
+++ app/src/components/albums/reducer.test.js
@@ -26,12 +26,24 @@ describe('Albums Reducer', () =>{
   it('adds an album to albums', () => {
     const data = { title: 'title', description: 'description', posterImage: 'url' };
     const newData = { title: 'newTitle', description: 'newDescription', posterImage: 'newUrl' };
-
+    
     const state = albums([data], {
       type: ALBUM_ADD,
       payload: newData
     });
-
+    
     expect(state).toEqual([data, newData]);
+  });
+});
+
+describe('selectors', () => {
+  
+  it('selects albums', () => {
+    const data = { title: 'title', description: 'description', posterImage: 'url' };
+    const newData = { title: 'newTitle', description: 'newDescription', posterImage: 'newUrl' };
+    
+    const albums = [data, newData];
+    const got = getAlbums({ albums });
+    expect(got).toBe(albums);
   });
 });
