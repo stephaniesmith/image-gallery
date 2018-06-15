566e10b49b4eacb2409f6b0404e5f34a2a484478
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-15 07:06:47 -0700

    wrote test for creates album action

--- app/src/components/albums/actions.test.js
+++ app/src/components/albums/actions.test.js
@@ -8,12 +8,23 @@ import { ALBUMS_LOAD, ALBUM_ADD } from './reducers';
 import { getAllAlbums, postAlbum } from '../../services/api';
 
 describe('albums actions', () => {
+
   it('loads albums', () => {
     const promise = Promise.resolve();
     getAllAlbums.mockReturnValueOnce(promise);
 
     const { type, payload } = loadAlbums();
     expect(type).toBe(ALBUMS_LOAD);
+    expect(getAllAlbums.mock.calls.length).toBe(1);
+    expect(payload).toBe(promise);
+  });
+
+  it('creates an albums', () => {
+    const promise = Promise.resolve();
+    postAlbum.mockReturnValueOnce(promise);
+
+    const { type, payload } = createAlbum(promise);
+    expect(type).toBe(ALBUMS_ADD);
     expect(getAllAlbums.mock.calls.length).toBe(1);
     expect(payload).toBe(promise);
   });
