ae809866210a7ccbd4464365b3221f3f8b9cbfe8
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-17 09:25:34 -0700

    settingup album detail

--- app/src/components/albums/AlbumDetail.js
+++ app/src/components/albums/AlbumDetail.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Thumbnail from '../thumbnail/Thumbnail';
 
-export default class AlbumDetail extends Component {
-  // static PropTypes = {
-  //   album
-  // }
+class AlbumDetail extends Component {
+
+  static PropTypes = {
+    album: PropTypes.object,
+    images: PropTypes.array
+  };
+
+  componentDidMount() {
+    //get the album
+    //get the images by albumId
+  }
 
   render() {
+    const { images } = this.props;
+    if(!images) return null;
+    
     return (
       <div>
-        I am a detail!
+        {images.map(image => <Thumbnail
+          key={image._id}
+          url={image.url}
+          title={image.title}/>)}
       </div>
     );
   }
 }
+
+export default connect(
+  // state => ({ albums: getAlbums(state) }),
+  // { loadAlbums }
+)(AlbumDetail);
