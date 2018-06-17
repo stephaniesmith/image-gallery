daa9d3ba432af4b2ff8c432d86de71a579136fab
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-17 10:01:23 -0700

    setting up the image detail

--- app/src/components/albums/AlbumDetail.js
+++ app/src/components/albums/AlbumDetail.js
@@ -3,31 +3,35 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getAlbum } from './reducers';
 import { loadAlbum } from './actions';
-import Thumbnail from '../thumbnail/Thumbnail';
+// import Thumbnail from '../thumbnail/Thumbnail';
 
 class AlbumDetail extends Component {
 
   static propTypes = {
     album: PropTypes.object,
-    images: PropTypes.array
+    images: PropTypes.array,
+    match: PropTypes.object
   };
 
   componentDidMount() {
-    this.props.loadAlbum();
+    console.log(this.props.match.params.id);
+    this.props.loadAlbum(this.props.match.params.id);
     //get the images by albumId
   }
 
   render() {
     const { images, album } = this.props;
-    // if(!album) return null;
+    const { title, description, posterImage } = album;
     
     return (
       <div>
-        {album ? <h1>{album.title}</h1> : <h1>No album</h1>}
-        {images.map(image => <Thumbnail
+        {title ? <h1>{title}</h1> : <h1>No title</h1>}
+        {description ? <h1>{description}</h1> : <h1>No description</h1>}
+        {posterImage ? <img src={posterImage}/> : <h1>No Cover Image</h1>}
+        {/* {images.map(image => <Thumbnail
           key={image._id}
           url={image.url}
-          title={image.title}/>)}
+          title={image.title}/>)} */}
       </div>
     );
   }
