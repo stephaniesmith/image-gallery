5a5ac0ef08d023d3146430a865b3f4518fed71ec
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-18 10:13:33 -0700

    added route for new image

--- app/src/components/app/App.js
+++ app/src/components/app/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './Header';
-import Footer from './Footer';
 import Albums from '../albums/Albums';
 import NewAlbum from '../albums/NewAlbum';
 import AlbumDetail from '../albums/AlbumDetail';
 import About from '../about/About';
 import Images from '../images/Images';
+import NewImage from '../images/NewImage';
 
 import styles from './App.css';
 
@@ -21,14 +21,14 @@ export default class App extends Component {
             <Switch>
               <Route exact path="/" component={Albums}/>
               <Route path="/albums/new" component={NewAlbum}/>
-              <Route path="/albums/:id" component={AlbumDetail}/>
+              <Route path="albums/:id/images/new" component={NewImage}/>
+              <Route path="/albums/:id/images/thumbnail" component={AlbumDetail}/>
               <Route path="/albums" component={Albums}/>
               <Route path="/about" component={About}/>
               <Route path="/images" component={Images}/>
               <Redirect to="/"/>
             </Switch>
           </main>
-          <Footer/>
         </div>
       </Router>
     );
