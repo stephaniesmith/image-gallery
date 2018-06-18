2aaa851a7332461854d59f01791a3a9fe4c8d534
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-18 16:58:40 -0700

    tested hash and password

--- server/test/unit/User.test.js
+++ server/test/unit/User.test.js
@@ -3,12 +3,27 @@ const User = require('../../lib/models/User');
 const { getErrors } = require('./helpers');
 
 describe('User model', () => {
-    it('valid and good model', () => {
-        const data = {
-            name: 'Steven',
-            email: 'steven@gemsrock.com'
-        };
+    const data = {
+        name: 'Steven',
+        email: 'steven@gemsrock.com'
+    };
 
+    const password = 'abc';
+
+    it('generates hash from password', () => {
+        const steven = new User(data);
+        steven.generateHash(password);
+        assert.ok(steven.hash);
+        assert.notEqual(steven.hash, password);
+    });
+
+    it('compares password to hash', () => {
+        const steven = new User(data);
+        steven.generateHash(password);
+        assert.isOk(steven.comparePassword(password)); 
+    });
+
+    it('valid and good model', () => {
         const steven = new User(data);
         data._id = steven._id;
         assert.deepEqual(steven.toJSON(), data);
@@ -16,8 +31,9 @@ describe('User model', () => {
 
     it('username is required', () => {
         const user = new User({});
-        const errors = getErrors(user.validateSync(), 2);
+        const errors = getErrors(user.validateSync(), 3);
         assert.equal(errors.name.kind, 'required');
         assert.equal(errors.email.kind, 'required');
+        assert.equal(errors.hash.kind, 'required');
     });
 });
