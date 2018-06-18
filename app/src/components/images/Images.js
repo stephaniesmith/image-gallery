5d7de2b8085067170dd449735c40db0b8c1cca8e
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-18 09:26:53 -0700

    images

--- app/src/components/images/Images.js
+++ app/src/components/images/Images.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getImages } from '../albums/reducers';
+import { loadImages } from '../albums/actions';
+import Thumbnail from '../thumbnail/Thumbnail';
+import styles from '../albums/Albums.css';
 
-export default class Images extends Component {
+class Images extends Component {
+
+  static propTypes = {
+    loadImages: PropTypes.func.isRequired,
+    images: PropTypes.array
+  };
+
+  componentDidMount() {
+    this.props.loadImages();
+  }
+
   render() {
+    const { images } = this.props;
+    if(!images) return null;
+
     return (
-      <div>
-        IMAGES!
+      <div className={styles.albums}>
+        {images.map(image => <Thumbnail key={image._id} url={image.url} title={image.title}/>)}
       </div>
     );
   }
 }
+
+export default connect(
+  state => ({ images: getImages(state) }),
+  { loadImages }
+)(Images);
