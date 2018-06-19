6e30b0359c55f67ac6f72952003b2278deb539c3
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-19 07:29:58 -0700

    added user auth type to reducer

--- app/src/components/auth/reducers.js
+++ app/src/components/auth/reducers.js
@@ -1,10 +1,12 @@
-
+export const USER_AUTH = 'USER_AUTH';
 
 
 
 
 export function user(state = null, { type, payload }) {
-  switch(type) {
+  switch (type) {
+    case USER_AUTH:
+      return payload;
     default:
       return state;
   }
