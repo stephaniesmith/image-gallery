af6b38f547ee4381dc71092ae51f6fa065f19d68
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-19 10:23:33 -0700

    working on header

--- app/src/components/app/Header.js
+++ app/src/components/app/Header.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getUser } from '../auth/reducers';
+import { logout } from '../auth/actions';
 
 import styles from './Header.css';
 
@@ -8,6 +13,17 @@ export default class Footer extends Component {
       <header className={styles.header}>
         <nav>
           <ul>
+          <Link to="/">Home</Link>
+          &nbsp;
+          <Link to="/pets">Pets</Link>
+          &nbsp;
+          {
+            user
+              ? <Link to="/" onClick={this.handleLogout}>Logout</Link>
+              : <Link to="/auth">Login</Link>
+          }
+
+
             <li><a href="/albums">
               Albums</a></li>
             <li><a href="/about">
