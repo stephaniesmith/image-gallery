146ac4cd1132fc939eb0e72a1ee8e6d5aeb12f6b
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-19 07:28:07 -0700

    user reducer and tests

---
+++ app/src/components/auth/reducers.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+export function user(state = null, { type, payload }) {
+  switch(type) {
+    default:
+      return state;
+  }
+}
--- app/src/components/auth/reducers.test.js
+++ app/src/components/auth/reducers.test.js
@@ -1,8 +1,20 @@
 import {
-  user
+  user,
+  USER_AUTH
 } from './reducers';
 
 describe('user reducer', () => {
-  const state = user(undefined, {});
-  expect(state).toBe(null);
+
+  it('initializes to null', () => {
+    const state = user(undefined, {});
+    expect(state).toBe(null);
+  });
+
+  it('loads a user', () => {
+    const data = { name: 'user' };
+    const state = user(null, { type: USER_AUTH, payload: data });
+    expect(state).toBe(data);
+  });
+
+
 });
