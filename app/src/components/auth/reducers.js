12237932e8cc85029fbacccc7d423073e1b884a0
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-19 07:33:29 -0700

    updated reducer

--- app/src/components/auth/reducers.js
+++ app/src/components/auth/reducers.js
@@ -1,12 +1,12 @@
 export const USER_AUTH = 'USER_AUTH';
-
-
-
+export const LOGOUT = 'LOGOUT';
 
 export function user(state = null, { type, payload }) {
   switch (type) {
     case USER_AUTH:
       return payload;
+    case LOGOUT:
+      return null;
     default:
       return state;
   }
