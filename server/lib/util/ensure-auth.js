72e5b8eeaee356fba09321b81f1bde87dafa39df
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-19 14:37:44 -0700

    added ensure auth file, missing res (?)

---
+++ server/lib/util/ensure-auth.js
@@ -0,0 +1,19 @@
+const tokenService = require('./token-service');
+
+module.exports = function(){
+    return(req, res, next) => {
+        const token = req.get('token');
+        try {
+            if(!token) return next({ status: 400, error: 'no token found' });
+
+            const payload = tokenService.verify(token);
+            req.user = payload;
+
+            next();
+        }
+
+        catch(err) {
+            next({ status: 401, error: 'Invalid Token' });
+        }
+    };
+};
