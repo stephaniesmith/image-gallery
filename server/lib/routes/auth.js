ae36b7b1a48cf771aeb21b98d98caf326720e155
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-18 17:13:01 -0700

    created auth route

---
+++ server/lib/routes/auth.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const { respond } = require('./route-helper');
+const User = require('../models/User');
+const { sign } = require('../util/token-service');
+const createEnsureAuth = require('../util/ensure-auth');
+
+
+const hasEmailAndPassword = ({ body }, res, next) => {
+    const { email, password, name } = body;
+    if(!email || !password || name ) {
+        throw {
+            status: 401,
+            error: 'Email, name, and password required'
+        };
+    }
+    next();
+};
+
+module.exports = router
+    .get('/verify', createEnsureAuth(), respond(
+        () => Promise.resolve({ verified: true })
+    ))
+
+    .post('/signup', hasEmailAndPassword, respond(
+        ({ body }) => {
+            const { name, email, password } = body;
+            delete body.password;
+
+            return User.exists({ name, email })
+                .then(exists => {
+                    if(exists) {
+                        throw {
+                            status: 400,
+                            error: 'Email exists'
+                        };
+                    }
+
+                    const user = new User(body);
+                    user.generateHash(password);
+                    return user.save();
+                })
+                .then(user => {
+                    return {
+                        token: sign(user),
+                        _id: user._id,
+                        roles: user.roles
+                    };
+                });
+        }
+    ))
+
+    .post('/signin', hasEmailAndPassword, respond(
+        ({ body }) => {
+            const { email, password } = body;
+            delete body.password;
+
+            return User.findOne({ email })
+                .then(user => {
+                    if(!user || !user.comparePassword(password)) {
+                        throw {
+                            status: 401,
+                            error: 'Invalid email or password'
+                        };
+                    }
+                    return { token: sign(user) };
+                });
+        }
+    ));
