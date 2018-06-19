a3b204ab2164986166a3972ab4e0935dc5947927
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-19 14:36:42 -0700

    wrote auth test for sign in , not passing

---
+++ server/test/e2e/auth.test.js
@@ -0,0 +1,27 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+
+describe('auth API', () => {
+    before(() => dropCollection( 'users'));
+    let token = null;
+
+    beforeEach(() => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                name: 'Happy Heart',
+                email: 'happyheart@yahoo.com',
+                password: 'heartattack'
+            })
+            .then(({ body }) => {
+                return token = body.token;
+            });
+    });
+
+    it('signin', () => {
+        assert.ok(token);
+    });
+
+});
+
