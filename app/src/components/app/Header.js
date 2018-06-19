7c720581609d6ccb7516f80ac7495164f4a5ec9e
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-19 10:27:21 -0700

    updated the header

--- app/src/components/app/Header.js
+++ app/src/components/app/Header.js
@@ -7,32 +7,42 @@ import { logout } from '../auth/actions';
 
 import styles from './Header.css';
 
-export default class Footer extends Component {
+class Header extends Component {
+
+  static propTypes = {
+    user: PropTypes.object,
+    logout: PropTypes.func.isRequired
+  };
+
+  handleLogout = () => {
+    this.props.logout();
+  };
+
   render() {
+    const { user } = this.props;
+
     return (
       <header className={styles.header}>
         <nav>
-          <ul>
-          <Link to="/">Home</Link>
+          <Link to="/albums">Albums</Link>
           &nbsp;
-          <Link to="/pets">Pets</Link>
+          <Link to="/about">About</Link>
+          &nbsp;
+          <Link to="/images">Images</Link>
           &nbsp;
           {
             user
               ? <Link to="/" onClick={this.handleLogout}>Logout</Link>
               : <Link to="/auth">Login</Link>
           }
-
-
-            <li><a href="/albums">
-              Albums</a></li>
-            <li><a href="/about">
-              About</a></li>
-            <li><a href="/images">
-              Images</a></li>
-          </ul>
         </nav>
+        { user && <span>Hello {user.name}!</span> }
       </header>
     );
   }
 }
+
+export default connect(
+  state => ({ user: getUser(state) }),
+  { logout }
+)(Header);
