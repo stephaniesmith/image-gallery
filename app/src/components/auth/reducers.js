b87fb7307d8d845c1f680e90f6189a87eb358732
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-19 07:35:59 -0700

    added user selector

--- app/src/components/auth/reducers.js
+++ app/src/components/auth/reducers.js
@@ -1,6 +1,8 @@
 export const USER_AUTH = 'USER_AUTH';
 export const LOGOUT = 'LOGOUT';
 
+export const getUser = state => state.user;
+
 export function user(state = null, { type, payload }) {
   switch (type) {
     case USER_AUTH:
