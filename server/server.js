612c0ff3c2831dffedaf0c0cb3a3b0939c4c1b1a
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-19 16:23:21 -0700

    simplified mongo server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
-require('dotenv').config();
 const http = require('http');
 const app = require('./lib/app');
 const connect = require('./lib/util/connect');
 
-const PORT = process.env.PORT;
-
-const MONGODB_URI = process.env.MONGODB_URI;
-
-connect(MONGODB_URI);
-
+const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 
+connect(process.env.MONGODB_URI) || 'mongodb://localhost:27017';
+
+
 server.listen(PORT, () => {
+    //eslint-disable-next-line
     console.log(`server listening on port`, server.address().port);
 });
